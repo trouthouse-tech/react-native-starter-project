67ccdd0454e4069b5eab0d62d4906a846ada3298
Edwards Moses <edwardsmoses3@gmail.com>
2021-10-19 15:18:40 +0100

    Moved source code to `template` folder in prep for use as project template.
    https://github.com/react-native-community/cli/blob/master/docs/init.md#creating-custom-template

---
+++ script.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log("You are ready to get started on this Project.");
---
+++ template.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "Starter Project",
+
+  titlePlaceholder: 'TroutHouse React Native',
+
+  // Directory with template
+  templateDir: "./template",
+
+  // Path to script, which will be executed after init
+  postInitScript: "./script.js"
+};
